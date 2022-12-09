58bd27867829c3478c28872a08df14ea10f627c9
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2022-12-08 22:29:49 -0500

    Primera entrega del proyecto final RILSA.S.A.S JUAN FONSECA VARGAS

---
+++ main.js
@@ -0,0 +1,43 @@
+// PRIMERA ENTRAGA PROYECTO FINAL 
+
+let datos = prompt("Para llenar el formulario escriba 1");
+while  (datos != 0 ){
+
+const nombre = prompt("Ingrese su nombre: ");
+const apellido = prompt("Ingrese su apellido: ");
+const correo = prompt("Ingrese su direccion de correo; ");
+const telefono = prompt("Ingrese su numero de celular: "); 
+
+    let entrada = prompt("Seleccionar equipo deseado 1 - Cellocator CR300 2 - Meitrack_T366 3 - Suntech_310U 4 - Teltonika_FMC130 ");
+    while (entrada != 0 ){
+
+    switch (entrada) {
+        case "1":
+        alert("Selecciono Cellocator CR300 ");
+            break;
+        case "2":
+        alert("Selecciono Meitrack_T366 ");
+            break;
+        case "3": 
+        alert("Selecciono Suntech_310U ");
+            break;
+        case "4":
+        alert("Selecciono Teltonika_FMC130 ");
+            break;
+        default:
+            alert("ERROR no eligio ninguna opcion valida");
+            break;
+    } 
+    entrada = prompt("Para confirmar escriba 0 - de lo contrario - Seleccionar equipo deseado 1 - Cellocator CR300 2 - Meitrack_T366 3 - Suntech_310U 4 - Teltonika_FMC130");
+}
+
+const comentario = prompt("Cuentenos en que le podemos ayudar: "); 
+datos = prompt("Para enviar el formulario escriba 0")
+
+function nombreCompleto (){
+    return "Nombre completo: "+  nombre + " " + apellido + "                                                               " + "Correo: " +  correo + "                                                                               " + "Telefono: " +  telefono + "                                                                       " + "Comentario adicional: " + comentario ; 
+}
+alert(nombreCompleto());
+
+}
+
