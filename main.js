7dd13d6ba6aaa672dff5ff4d6e61392e67996c93
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2022-12-27 22:17:12 -0500

    Segunda entrega del proyecto final RilILSA S.A.S Juan Fonseca Vargas

--- main.js
+++ main.js
@@ -3,41 +3,50 @@
 let datos = prompt("Para llenar el formulario escriba 1");
 while  (datos != 0 ){
 
-const nombre = prompt("Ingrese su nombre: ");
-const apellido = prompt("Ingrese su apellido: ");
-const correo = prompt("Ingrese su direccion de correo; ");
-const telefono = prompt("Ingrese su numero de celular: "); 
+    let nombre = prompt("Ingrese su nombre: ");
+    let apellido = prompt("Ingrese su apellido: ");
+    let correo = prompt("Ingrese su direccion de correo; ");
+    let telefono = prompt("Ingrese su numero de celular: ");  
+
+    let userData = [];
+
+    function agregarDatos() {
+    let user = {nombre, apellido, correo, telefono}
+    userData.push(user)
+    
+    }
+
+    agregarDatos();
 
     let entrada = prompt("Seleccionar equipo deseado 1 - Cellocator CR300 2 - Meitrack_T366 3 - Suntech_310U 4 - Teltonika_FMC130 ");
     while (entrada != 0 ){
 
-    switch (entrada) {
+    switch (entrada) {   
         case "1":
         alert("Selecciono Cellocator CR300 ");
+        let selc1 = console.log("Selecciono Cellocator CR300 ")
             break;
         case "2":
         alert("Selecciono Meitrack_T366 ");
+        let selc2 = console.log("Selecciono Meitrack_T366 ")
             break;
         case "3": 
         alert("Selecciono Suntech_310U ");
+        let selc3 = console.log("Selecciono Cellocator CR300 ")
             break;
         case "4":
         alert("Selecciono Teltonika_FMC130 ");
+        let selc4 = console.log("Selecciono Teltonika_FMC130 ")
             break;
         default:
             alert("ERROR no eligio ninguna opcion valida");
             break;
     } 
     entrada = prompt("Para confirmar escriba 0 - de lo contrario - Seleccionar equipo deseado 1 - Cellocator CR300 2 - Meitrack_T366 3 - Suntech_310U 4 - Teltonika_FMC130");
-}
+    }
 
-const comentario = prompt("Cuentenos en que le podemos ayudar: "); 
-datos = prompt("Para enviar el formulario escriba 0")
-
-function nombreCompleto (){
-    return "En los proximos minutos un integrante de nuestro equipo se comunicara con usted y le enviaremos un correo" + "                   " + "Nombre completo: "+  nombre + " " + apellido + " " + "Correo: " +  correo + " " + "Telefono: " +  telefono + " " + "Comentario adicional: " + comentario ; 
-}
-alert(nombreCompleto());
-
+    const comentario = prompt("Cuentenos en que le podemos ayudar: "); 
+    datos = prompt("Para enviar el formulario escriba 0")
+    alert("En los proximos minutos uno de los miembros de nuestro equipo de atencion del cliente le enviara un correo con los siguientes datos: " + "Nombre completo: "+  nombre + apellido + "                    " + "Correo: " +  correo + "                                              " + "Telefono: " +  telefono + "                                                      " + "Comentario adicional: " + comentario );
 }
 
