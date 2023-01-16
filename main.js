494012fc4ea6a89a0871c8e6cf65fe72fe0f786d
Juan Felipe Fonseca Vargas <juanffonsecav5@gmail.com>
2023-01-15 23:20:21 -0500

    Tercera entrega del proyecto final, faltante de algunos cambios

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// PRIMERA ENTRAGA PROYECTO FINAL 
+/* // PRIMERA ENTRAGA PROYECTO FINAL 
 
 let datos = prompt("Para llenar el formulario escriba 1");
 while  (datos != 0 ){
 
-    let nombre = prompt("Ingrese su nombre: ");
+    let nombre = document.getElementById("nombre")
     let apellido = prompt("Ingrese su apellido: ");
     let correo = prompt("Ingrese su direccion de correo; ");
     let telefono = prompt("Ingrese su numero de celular: ");  
@@ -49,4 +49,55 @@ while  (datos != 0 ){
     datos = prompt("Para enviar el formulario escriba 0")
     alert("En los proximos minutos uno de los miembros de nuestro equipo de atencion del cliente le enviara un correo con los siguientes datos: " + "Nombre completo: "+  nombre + apellido + "                    " + "Correo: " +  correo + "                                              " + "Telefono: " +  telefono + "                                                      " + "Comentario adicional: " + comentario );
 }
+
+ */
+
+// TERCERA ENTREGA DEL PROYECTO FINAL
+
+const formContacto = document.getElementById('formContacto');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const email = document.getElementById('correo');
+const celular = document.getElementById('celular');
+const opciones = document.getElementById('opciones');
+const coments = document.getElementById('coments')
+let datos;
+
+
+//Condicional con operador ternario
+
+sessionStorage.getItem('datos') ? datos = JSON.parse(sessionStorage.getItem('datos')) : datos = [];
+
+//Datos del usuario
+
+class Datos {
+    constructor(nombre, apellido, email, celular){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.celular = celular;
+    }
+}
+
+//Guardado de datos
+
+function dataSave(data) {
+    return datos.push(data)
+}
+
+//Guardar en SS
+
+function saveData(array) {
+    return sessionStorage.setItem('datos' , JSON.stringify(array))
+}
+
+//Evento
+
+formContacto.addEventListener('submit', (submit)=>{
+submit.preventDefault();
+const newData = new Datos (nombre.value, apellido.value, email.value, celular.value);
+console.log(`Nombre: ${newData.nombre} Apellido: ${newData.apellido} Correo: ${newData.email} Celular: ${newData.celular} Equipo seleccionado: ${opciones.value} Comentario adicional: ${coments.value} `); 
+})
+
+//Selectores
 
